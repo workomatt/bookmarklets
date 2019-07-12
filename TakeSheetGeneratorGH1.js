2d25fdc1a5307d175a7300c9644ab3c933b6aecb
workomatt <51997495+workomatt@users.noreply.github.com>
2019-07-12 15:26:34 +0100

    Update TakeSheetGeneratorGH1.js

--- TakeSheetGeneratorGH1.js
+++ TakeSheetGeneratorGH1.js
@@ -1,16 +1,17 @@
 (function(){
 var bill = getText("div[aria-label^='Bill,']>div>div>button");
-var take = getText("div[aria-label^='Take']>div>span");
-var email = getText("div[aria-label^='Title']>div>span");
-var received = getText("div[aria-label^='Received']>div>span");
-var sentby = getText("div[aria-label^='Submitted by']>div>span");
+var take = getText("div[aria-label^='Take']>div");
+var email = getText("div[aria-label^='Title']>div");
+var received = getText("div[aria-label^='Received']>div");
+var sentby = getText("div[aria-label^='Submitted by']>div");
 
-var amdStart = getText("div[aria-label^='AMD start']>div>span");
-var amdEnd = getText("div[aria-label^='AMD end']>div>span");
-var ncStart = getText("div[aria-label^='NC start']>div>span");
-var ncEnd = getText("div[aria-label^='NC end']>div>span");
-var nsStart = getText("div[aria-label^='NS start']>div>span");
-var nsEnd = getText("div[aria-label^='NS end']>div>span");
+
+var amdStart = getText("div[aria-label^='AMD start']>div");
+var amdEnd = getText("div[aria-label^='AMD end']>div");
+var ncStart = getText("div[aria-label^='NC start']>div");
+var ncEnd = getText("div[aria-label^='NC end']>div");
+var nsStart = getText("div[aria-label^='NS start']>div");
+var nsEnd = getText("div[aria-label^='NS end']>div");
 
 
 function getText(query){
@@ -61,9 +62,9 @@ if (Number(nsStart) == 0) {
     }
 
 var files;
-console.log(document.querySelector("div[aria-label^='Files']>div>span"));
+console.log(document.querySelector("div[aria-label^='Files']>div"));
 
-if (document.querySelector("div[aria-label^='Files']>div>span").textContent == 'Yes'){
+if (document.querySelector("div[aria-label^='Files']>div").textContent == 'Yes'){
 	files = '';
 	var attachments =  document.querySelectorAll("div.ReactFieldEditor-Attachments-Renderer>a");
 	console.log(attachments);
@@ -80,13 +81,13 @@ var totalamds = Number(amdTotal) + Number(ncTotal) + Number(nsTotal);
 
 var star;
 
-if (document.querySelector("div[aria-label^='Star']>div>span").textContent == 'Yes'){
+if (document.querySelector("div[aria-label^='Star']>div").textContent == 'Yes'){
     star = "Please star &#x2605;"
 } else {
     star = "Please <i>do not</i> star"
 }
 
-var html = "<html lang='en'><head><meta charset='UTF-8'><title>Amendment Take Sheet</title><style>html{font-family:'Frutiger LT Std',Verdana,Helvetica,Arial,sans-serif;}.center{text-align:center;}.right{text-align:right;}.takeVal{font-size:34pt;}.titleBlock{text-align:center;margin-bottom:20%;}h3{text-align:center;}h4{text-align:center;}.emailTitle{margin-bottom:10%;}.amendsBlock{margin-bottom:20%;}.bigger{font-size:x-large}.tab{margin:0 auto;}}.lastRow{margin-bottom:10px;}</style></head><body><div class='headerBlock'><table width='100%'><colgroup><col width='50%'/><col width='50%'/></colgroup><tbody><tr><td class='paperLabel'>Paper</td><td class='right takeLabel'>Take</td></tr><tr><td class='paperVal'/><td class='right takeVal'>" + take + "</td></tr></tbody></table></div><div class='titleBlock'><h3>Amendment Take Sheet</h3><h2>" + bill + "</h2></div><div class='amendsBlock'><h2 class='center'>" + totalamds + " amendment(s) in total</h2><table class='tab'><tbody><tr><td class='bigger center'>Amds = " + amdTotal + "</td></tr><tr><td class='bigger center'>NC = " + ncTotal + "</td></tr><tr><td class='bigger center'>NS = " + nsTotal + "</td></tr></tbody></table><p class='center'>(" + star + ")</p></div><div class='filesBlock'><table width='90%' class='tab'><colgroup><col width='50%'/><col width='50%'/></colgroup><tbody><tr><td colspan='2'><b>See email:</b></td></tr><tr><td colspan='2' class='emailTitle'>" + email + "</td></tr><tr><td colspan='2'>&nbsp;</td></tr><tr><td colspan='2'><b>Attached file(s):</b> " + files + "</td></tr><tr><td colspan='2'>&nbsp;</td></tr><tr><td><b>Received at:</b></td><td><b>Sent by:</b></td></tr><tr><td>" + received + "</td><td>" + sentby + "</td></tr></tbody></table></div></body></body></body></html>";
+var html = "<html lang='en'><head><meta charset='UTF-8'><title>Amendment Take Sheet</title><style>html{font-family:'Frutiger LT Std',Verdana,Helvetica,Arial,sans-serif;}.center{text-align:center;}.right{text-align:right;}.takeVal{font-size:44pt;}.titleBlock{text-align:center;margin-bottom:20%;}h3{text-align:center;}h4{text-align:center;}.emailTitle{margin-bottom:10%;}.amendsBlock{margin-bottom:20%;}.bigger{font-size:x-large}.tab{margin:0 auto;}}.lastRow{margin-bottom:10px;}.headerBlock{margin-bottom:10%;}</style></head><body><div class='headerBlock'><table width='100%'><colgroup><col width='50%'/><col width='50%'/></colgroup><tbody><tr><td class='paperLabel'></td><td class='right takeLabel'>TAKE</td></tr><tr><td class='paperVal'></td><td class='right takeVal'>" + take + "</td></tr></tbody></table></div><div class='titleBlock'><h3>Amendment Take Sheet</h3><h2>" + bill + "</h2></div><div class='amendsBlock'><h2 class='center'>" + totalamds + " amendment(s) in total</h2><table class='tab'><tbody><tr><td class='bigger center'>Amds = " + amdTotal + "</td></tr><tr><td class='bigger center'>NC = " + ncTotal + "</td></tr><tr><td class='bigger center'>NS = " + nsTotal + "</td></tr></tbody></table><p class='center'>(" + star + ")</p></div><div class='filesBlock'><table width='90%' class='tab'><colgroup><col width='50%'/><col width='50%'/></colgroup><tbody><tr><td colspan='2'><b>See email:</b></td></tr><tr><td colspan='2' class='emailTitle'>" + email + "</td></tr><tr><td colspan='2'>&nbsp;</td></tr><tr><td colspan='2'><b>Attached file(s):</b> " + files + "</td></tr><tr><td colspan='2'>&nbsp;</td></tr><tr><td><b>Received at:</b></td><td><b>Sent by:</b></td></tr><tr><td>" + received + "</td><td>" + sentby + "</td></tr></tbody></table></div></body></body></body></html>";
 var myWindow = (window.open("", "", "width=900, height=1050"));
 var writeOutput = myWindow.document.write(html);
 myWindow.print();
